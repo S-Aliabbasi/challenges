9fd078654457041df47ba46114a7368a9fc83bcc
s-Aliabbasi <sona@aliabbasi>
2022-12-13 11:31:34 +0100

    add rest coffe

---
+++ site/app.js
@@ -0,0 +1,9 @@
+const Bname = ["ali", "fateme", "maryam", "amin"];
+let Lastname = "moharrami";
+let FullName = [];
+
+console.log("sona");
+for (let i = Bname.length; i == 0; i--) {
+  FullName = `${Bname} ${Lastname}`;
+  console.log(FullName);
+}
