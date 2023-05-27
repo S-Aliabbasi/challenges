b1d94ca53418e495fc98263702304d0a37b684ab
s-Aliabbasi <sona@aliabbasi>
2023-05-27 10:27:23 +0200

    remove some item

--- challenge/bot.js
+++
@@ -1,8 +0,0 @@
-function PickJoke(category) {
-  document.getElementById("demo").innerHTML =
-    Jokes[Math.floor(Math.random() * 10) + category];
-}
-function myFunction(button) {
-  var x = button.id;
-  PickJoke(parseInt(x));
-}
--- site/app.js
+++
@@ -1,7 +0,0 @@
-function calc(num, num1) {
-  //const golobalnum=20;
-  let result = num + num1;
-  return result;
-}
-
-console.log(calc("A", [2, 5, 7]));
